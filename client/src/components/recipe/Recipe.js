c4e5d67714bdd2467f707ac02e87f657cc2cef51
8PJ <pijus.stockus@gmail.com>
2022-09-20 19:33:37 +0100

    make recipe wall

--- client/src/components/recipe/Recipe.js
+++ client/src/components/recipe/Recipe.js
@@ -1,23 +1,31 @@
 import Container from "react-bootstrap/esm/Container";
+import Button from "react-bootstrap/esm/Button";
 
 function Recipe(props) {
     return (
-        <Container className="recipeBox">
-            <h1 className="recipeName">Recipe Name</h1>
+        <Container className="recipeBox" fluid>
+            <h1 className="recipeName">{props.name}</h1>
             <hr className="recipeDivider" />
             <Container className="recipeIngredients">
                 <ul>
-                    <li>Ingredient 1</li>
-                    <li>Ingredient 2</li>
-                    <li>Ingredient 3</li>
+                    {props.ingredients.map((ingredient, index) => (
+                        <li key={index}>{ingredient}</li>
+                    ))}
                 </ul>
             </Container>
             <hr className="recipeDivider" />
             <Container className="recipeDescription">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                {props.description.slice(0, 99) + "..."}
+            </Container>
+            <Container className="d-flex justify-content-center">
+                <Button
+                    variant="outline-dark"
+                    type="button"
+                    className="recipeExpandButton"
+                    size="lg"
+                >
+                    Full Recipe
+                </Button>
             </Container>
         </Container>
     );
--- client/src/routes/Recipes.js
+++ client/src/routes/Recipes.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 
 import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import RecipeBreadcrumb from "../components/recipe/RecipeBreadcrumb";
 import RecipeDropdown from "../components/recipe/RecipeDropdown";
 import Recipe from "../components/recipe/Recipe";
@@ -10,8 +13,7 @@ function Recipes(props) {
     const [isDropdown, setIsDropdown] = useState(window.innerWidth <= 464);
 
     useEffect(() => {
-        const handleWindowResize = () =>
-            setIsDropdown(window.innerWidth <= 464);
+        const handleWindowResize = () => setIsDropdown(window.innerWidth <= 464);
 
         window.addEventListener("resize", handleWindowResize);
 
@@ -25,9 +27,63 @@ function Recipes(props) {
             <Container className="recipeNavigation d-flex justify-content-center">
                 {isDropdown ? <RecipeDropdown /> : <RecipeBreadcrumb />}
             </Container>
-            <Container>
-                <Recipe />
-            </Container>
+            <Row className="recipeWall">
+                <Col xs={12} sm={6} lg={4} xl={3}>
+                    <Recipe
+                        recipeID={1}
+                        name="Recipe name"
+                        ingredients={["Ingredient1", "Ingredient2", "Ingredient3"]}
+                        description="Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book."
+                    />
+                </Col>
+                <Col xs={12} sm={6} lg={4} xl={3}>
+                    <Recipe
+                        recipeID={1}
+                        name="Recipe name"
+                        ingredients={["Ingredient1", "Ingredient2", "Ingredient3"]}
+                        description="Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book."
+                    />
+                </Col>
+                <Col xs={12} sm={6} lg={4} xl={3}>
+                    <Recipe
+                        recipeID={1}
+                        name="Recipe name"
+                        ingredients={["Ingredient1", "Ingredient2", "Ingredient3"]}
+                        description="Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book."
+                    />
+                </Col>
+                <Col xs={12} sm={6} lg={4} xl={3}>
+                    <Recipe
+                        recipeID={1}
+                        name="Recipe name"
+                        ingredients={["Ingredient1", "Ingredient2", "Ingredient3"]}
+                        description="Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book."
+                    />
+                </Col>
+                <Col xs={12} sm={6} lg={4} xl={3}>
+                    <Recipe
+                        recipeID={1}
+                        name="Recipe name"
+                        ingredients={["Ingredient1", "Ingredient2", "Ingredient3"]}
+                        description="Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book."
+                    />
+                </Col>
+            </Row>
         </>
     );
 }
