ee1aea21bc2ac976fe7d266690eda711736e2e9d
8PJ <pijus.stockus@gmail.com>
2022-09-27 14:33:03 +0100

    add routing to navbar

--- client/src/components/NavbarComp.js
+++ client/src/components/NavbarComp.js
@@ -1,17 +1,25 @@
+import { Link } from "react-router-dom";
+
 import Navbar from "react-bootstrap/Navbar";
-import Nav from 'react-bootstrap/Nav';
+import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
 function NavbarComp(props) {
     return (
-        <Navbar collapseOnSelect expand="md" variant="dark" className="mainNav" >
+        <Navbar collapseOnSelect expand="md" variant="dark" className="mainNav">
             <Container fluid className="p-1 ps-5">
-                <Navbar.Brand className="logo" href="/">MealUp</Navbar.Brand>
+                <Navbar.Brand className="logo">
+                    <Link to="/">MealUp</Link>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="navLinks justify-content-end" id="responsive-navbar-nav">
                     <Nav>
-                        <a className="navLink" href="#Register">Register</a>
-                        <a className="navLink" href="#Login">Login</a>
+                        <Link className="navLink" to="Register">
+                            Register
+                        </Link>
+                        <Link className="navLink" to="Login">
+                            Login
+                        </Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
